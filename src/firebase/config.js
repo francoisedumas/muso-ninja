f16ca131183858f181030c20c40f96c651b36410
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-11-06 21:55:36 +0100

    Setup firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKo2UJMBX0NB4SCpThUY7dtG9Zo2OiBUQ",
+  authDomain: "muso-ninja-e0ff1.firebaseapp.com",
+  projectId: "muso-ninja-e0ff1",
+  storageBucket: "muso-ninja-e0ff1.appspot.com",
+  messagingSenderId: "452464070941",
+  appId: "1:452464070941:web:1c896c5a627ef923e651f4"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, projectAuth, timestamp }
