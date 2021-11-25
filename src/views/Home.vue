ceced50acba26ab3822826966ffe0072d8c5daaf
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-11-25 22:08:04 +0100

    display all playlists

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">
-    <p>Homepage</p>
+    <div v-if="error" class="error">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="document in documents" :key="document.id">
+        <h2>{{ document.title }}</h2>
+        <p>{{ document.description }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    const { error, documents } = getCollection('playlists')
+
+    return { error, documents }
+  }
 }
 </script>
